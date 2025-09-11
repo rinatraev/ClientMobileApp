0b3040f47918df0e6bec747fbf06e2ac4859ee89
rinatraev <64296176+rinatraev@users.noreply.github.com>
2025-09-11 20:03:09 +0500

    Initial commit: MAUI project setup

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ClientMobileApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ClientMobileApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
